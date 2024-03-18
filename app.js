b8b545a2b1ae03632d2c3e183c7be9140b480696
ianshaloom <ianshaloom0@outlook.com>
2024-03-18 21:20:38 +0300

    version 1.0.6

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import cors from "cors";
 
 process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
-const baseUrl = "https://dart-frog-backend-qcsrtff-ianshaloom.globeapp.dev";
+const baseUrl = "https://dart-frog-backend-axoq040-ianshaloom.globeapp.dev";
 const endpoint = "/api/token";
 const url = baseUrl + endpoint;
 
@@ -55,7 +55,7 @@ app.post("/send", async function (req, res) {
       return tokens;
     } catch (error) {
       console.error("Error fetching tokens:", error);
-      // Consider returning an error object here
+      // Consider returning an error object here1
     }
   };
 
